1c0e1dca566340fd230f08ad0ddb576018d17d5f
cike2056 <whzhn1990@163.com>
2014-12-04 16:19:51 +0800

    在接口,重写onResponse方法中的JSONObject是要获得的JSONObject.

--- src/com/qf/example_volley/MainActivity.java
+++ src/com/qf/example_volley/MainActivity.java
@@ -1,34 +1,46 @@
 package com.qf.example_volley;
 
+import org.json.JSONObject;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Request.Method;
+import com.android.volley.Response.Listener;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements Listener<JSONObject>{
+	private RequestQueue mQueue;
+	private TextView tx;
+	String url = "http://news-at.zhihu.com/api/3/stories/latest";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		init();
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	private void init() {
+		// TODO Auto-generated method stub
+		tx=(TextView) findViewById(R.id.tx);
+		mQueue = Volley.newRequestQueue(getApplicationContext());
+		getJSOnObject();
 	}
-
+	private void getJSOnObject() {
+		// TODO Auto-generated method stub
+		Request<JSONObject> requestObj = mQueue.add(new JsonObjectRequest(Method.GET,url, null, this, null));
+	}
+	/**
+	 * 重写onResponse方法中的JSONObject是要获得的JSONObject.
+	 */
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+	public void onResponse(JSONObject jsonObject) {
+		// TODO Auto-generated method stub
+		tx.setText(jsonObject.toString());
 	}
 }
